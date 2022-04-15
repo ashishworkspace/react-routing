36227607de6a0ce41524850f50ef47a396a2a11d
ashish <get.mail.ashish@gmail.com>
2022-04-15 12:44:32 +0530

    concept of queryString

--- src/components/pages/Items.jsx
+++ src/components/pages/Items.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-
+import React from "react";
+import { useParams, useSearchParams } from "react-router-dom";
 
 const Items = () => {
-  const {category} = useParams();
+  const { category } = useParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+  console.log(searchParams)
   return (
-    <div>Items = {category}</div>
-  )
-}
+    <>
+      <div>Items = {category}</div>
+      <div>QueryString = {searchParams.get('price')}</div>
+    </>
+  );
+};
 
-export default Items
+export default Items;
